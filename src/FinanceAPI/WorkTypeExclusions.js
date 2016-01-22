bf19c7963d4e8d9283fb7319dc83368462c99680
kgrube <grube.kevin@gmail.com>
2016-01-22 15:14:48 -0800

    FinanceAPI/WorkTypeExclusions initial commit

---
+++ src/FinanceAPI/WorkTypeExclusions.js
@@ -0,0 +1,77 @@
+/**
+ * Created by kgrube on 1/22/2016.
+ */
+
+/**
+ * @private
+ */
+var Q = require('q'),
+    inherits = require('util').inherits,
+    ConnectWise = require('../ConnectWise.js');
+
+/**
+ * @typedef {object} WorkTypeExclusion
+ * @property {number} id
+ * @property {WorkTypeHref} workType
+ * @property {number} agreementId
+ * @property {object} _info
+ * @property {string} _info.lastUpdated
+ * @property {string} _info.updatedBy
+ */
+
+/**
+ *
+ * @param {CWOptions} options
+ * @inherits {ConnectWise}
+ * @constructor
+ */
+function WorkTypeExclusions(options) {
+    ConnectWise.apply(this, Array.prototype.slice.call(arguments));
+}
+inherits(WorkTypeExclusions, ConnectWise);
+
+/**
+ * GET
+ * @param agreementId
+ * @param {Params} params
+ * @returns {promise|WorkTypeExclusion[]}
+ */
+WorkTypeExclusions.prototype.getWorkTypeExclusions = function (agreementId, params) {
+    return this.api('/finance/agreements/' + agreementId + '/workTypeExclusions', 'GET', params);
+};
+
+/**
+ * POST
+ * @param agreementId
+ * @param {WorkTypeExclusion} workTypeExclusion
+ * @returns {promise|WorkTypeExclusion}
+ */
+WorkTypeExclusions.prototype.createWorkTypeExclusion = function (agreementId, workTypeExclusion) {
+    return this.api('/finance/agreements/' + agreementId + '/workTypeExclusions', 'POST', workTypeExclusion);
+};
+
+/**
+ * GET
+ * @param agreementId
+ * @param {ParamsConditions} params
+ * @returns {promise|Count}
+ */
+WorkTypeExclusions.prototype.getWorkTypeExclusionsCount = function (agreementId, params) {
+    return this.api('/finance/agreements/' + agreementId + '/workTypeExclusions/count', 'GET', params);
+};
+
+/**
+ * DELETE
+ * @param agreementId
+ * @param workTypeExclusionId
+ */
+WorkTypeExclusions.prototype.deleteWorkTypeExclusionById = function(agreementId, workTypeExclusionId){
+    return this.api('/finance/agreements/' + agreementId + '/workTypeExclusions/' + workTypeExclusionId, 'DELETE');
+};
+
+
+/**
+ *
+ * @type {WorkTypeExclusions}
+ */
+module.exports = WorkTypeExclusions;
