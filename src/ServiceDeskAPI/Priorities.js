03fa866b7e06cfc6c61fcfa195a3fa03fd33555e
Kevin Grube <grube.kevin@gmail.com>
2016-06-27 00:21:11 -0700

    Add ServiceDeskAPI/Priorities

--- src/ServiceDeskAPI/Priorities.js
+++ src/ServiceDeskAPI/Priorities.js
@@ -1,26 +1,22 @@
 /**
  * Created by kgrube on 12/24/2015.
  * @author kgrube
+ * @TODO complete module
  */
 
 /**
  * @private
  */
-var Q = require('q'),
-    inherits = require('util').inherits,
+var inherits = require('util').inherits,
     ConnectWise = require('../ConnectWise.js');
 /**
- * @typedef {object} Board
+ * @typedef {object} Priority
  * @property {number} id
  * @property {string} name
- * @property {number} locationId
- * @property {number} businessUnitId
- * @property {boolean} inactive
- * @property {number} signOffTemplateId
- * @property {boolean} sendToContact
- * @property {number} contactTemplateId
- * @property {boolean} sendToResource
- * @property {number} resourceTemplateId
+ * @property {string} color
+ * @property {number} sortOrder
+ * @property {boolean} defaultFlag
+ * @property {string} imageLink
  * @property {object} _info
  *
  */
@@ -34,69 +30,22 @@ var Q = require('q'),
  * @param {string} options.companyUrl
  * @constructor
  */
-function Boards(options) {
+function Priorities(options) {
     ConnectWise.apply(this, Array.prototype.slice.call(arguments));
 }
-inherits(Boards, ConnectWise);
+inherits(Priorities, ConnectWise);
 
 /**
- * GET
+ *
  * @param {Params} params
- * @returns {Board[]|promise}
+ * @returns {promise|Priority[]}
  */
-Boards.prototype.getBoards = function (params) {
-    return this.api('/service/boards', 'GET', params);
-};
-
-/**
- * POST
- * @param {Board} board
- * @returns {Board[]|promise}
- */
-Boards.prototype.createBoard = function (board) {
-    return this.api('/service/boards', 'POST', board);
-};
-
-/**
- * GET
- * @param id
- * @returns {Board[]|promise}
- */
-Boards.prototype.getBoardById = function (id) {
-    return this.api('/service/boards/' + id, 'GET');
-};
-
-/**
- * GET
- * @param {ParamsConditions} params
- * @returns {Count|promise}
- */
-Boards.prototype.getBoardsCount = function (params) {
-    return this.api('/service/boards/count', 'GET', params);
-};
-
-/**
- * PATCH
- * @param id
- * @param {Operations} ops
- * @returns {Board|promise}
- */
-Boards.prototype.updateBoard = function (id, ops) {
-    return this.api('/service/boards/' + id, 'PATCH', ops);
+Priorities.prototype.getPriorities = function (params) {
+    return this.api('/service/priorities', 'GET', params);
 };
 
 /**
  *
- * @param id
- * @param {Board} board
- * @returns {Board|promise}
+ * @type {Priorities}
  */
-Boards.prototype.replaceBoard = function (id, board) {
-    return this.api('/service/boards/' + id, 'PUT', board);
-};
-
-/**
- *
- * @type {Boards}
- */
-module.exports = Boards;
+module.exports = Priorities;
