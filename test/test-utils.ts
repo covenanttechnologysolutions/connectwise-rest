771a4ff1ed445790fd6f0df774eed9ff8792c02a
k-grube <grube.kevin@gmail.com>
2022-03-25 11:24:48 -0700

    update test

--- test/test-utils.ts
+++ test/test-utils.ts
@@ -1,8 +1,17 @@
-import { types } from 'util'
 export type PromiseArray<T> = Array<Promise<T>>
 
+export function isPromise(input: unknown): input is Promise<unknown> {
+  return (
+    !!input &&
+    (typeof input === 'object' || typeof input === 'function') &&
+    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+    // @ts-ignore
+    typeof input.then === 'function'
+  )
+}
+
 export function isPromiseType(input: unknown): input is Promise<unknown> {
-  return types.isPromise(input)
+  return isPromise(input)
 }
 
 export function isArrayOfPromises(input: unknown): input is PromiseArray<unknown> {
@@ -10,7 +19,7 @@ export function isArrayOfPromises(input: unknown): input is PromiseArray<unknown
     if (Array.length === 0) {
       return true
     }
-    if (types.isPromise(input.pop)) {
+    if (isPromise(input.pop)) {
       return true
     }
   }
